ba0573a1f5777a19e52b5f6ed89513393ef17bb5
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-06-15 04:08:59 -0700

    refactor: simplify the getBookByTitle method by filtering and streams on path parameter

--- src/main/java/io/github/backendbaz/books/controller/BookController.java
+++ src/main/java/io/github/backendbaz/books/controller/BookController.java
@@ -39,9 +39,10 @@ public class BookController {
     // GET -> http://localhost:8080/api/books/C%23 (C#) {title}
     @GetMapping("/api/books/{title}")
     public Book getBookByTitle(@PathVariable String title) {
-        for (Book book : books)
-            if (book.getTitle().equalsIgnoreCase(title)) return book;
-        return null;
+        return books.stream()
+                .filter(book -> book.getTitle().equalsIgnoreCase(title))
+                .findFirst()
+                .orElse(null);
     }
 
 }
